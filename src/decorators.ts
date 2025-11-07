5479d96416adc672fa2430531702160d13416dd0
tvenceslau <venceslau.tiago@gmail.com>
2025-11-07 17:16:09 +0000

    patch - DECAF-178 transactionality working-no-ci - after release preparation

--- src/decorators.ts
+++ src/decorators.ts
@@ -1,5 +1,5 @@
 import { TransactionalKeys } from "./constants";
-import { Metadata, method } from "@decaf-ts/decoration";
+import { Decoration, Metadata, method } from "@decaf-ts/decoration";
 import { Transaction } from "./Transaction";
 import { InternalError } from "@decaf-ts/db-decorators";
 
@@ -36,104 +36,113 @@ import { InternalError } from "@decaf-ts/db-decorators";
  * @category Decorators
  */
 export function transactional(...data: any[]) {
-  return function (target: any, propertyKey?: any, descriptor?: any) {
-    if (!descriptor)
-      throw new InternalError("This decorator only applies to methods");
-    method()(target, propertyKey, descriptor);
-    Metadata.set(
-      target.constructor,
-      Metadata.key(TransactionalKeys.TRANSACTIONAL, propertyKey),
-      {
-        data: data,
-      }
-    );
-    descriptor.value = new Proxy(descriptor.value, {
-      async apply<R>(obj: any, thisArg: any, argArray: any[]): Promise<R> {
-        return new Promise<R>((resolve, reject) => {
-          async function exitFunction(
-            transaction: Transaction<R>,
-            err?: Error | R,
-            result?: R
-          ): Promise<R> {
-            if (err && !(err instanceof Error) && !result) {
-              result = err;
-              err = undefined;
+  function innerTransactional(...data: any[]) {
+    return function (target: any, propertyKey?: any, descriptor?: any) {
+      if (!descriptor)
+        throw new InternalError("This decorator only applies to methods");
+      method()(target, propertyKey, descriptor);
+      Metadata.set(
+        target.constructor,
+        Metadata.key(TransactionalKeys.TRANSACTIONAL, propertyKey),
+        {
+          data: data,
+        }
+      );
+      descriptor.value = new Proxy(descriptor.value, {
+        async apply<R>(obj: any, thisArg: any, argArray: any[]): Promise<R> {
+          return new Promise<R>((resolve, reject) => {
+            async function exitFunction(
+              transaction: Transaction<R>,
+              err?: Error | R,
+              result?: R
+            ): Promise<R> {
+              if (err && !(err instanceof Error) && !result) {
+                result = err;
+                err = undefined;
+              }
+              await transaction.release(err as Error | undefined);
+              return err
+                ? (reject(err) as unknown as R)
+                : (resolve(result as R) as unknown as R);
             }
-            await transaction.release(err as Error | undefined);
-            return err
-              ? (reject(err) as unknown as R)
-              : (resolve(result as R) as unknown as R);
-          }
 
-          const candidate = argArray[0];
-          const transactionPrefixLength = (() => {
-            let count = 0;
-            while (
-              count < argArray.length &&
-              argArray[count] instanceof Transaction
-            ) {
-              count++;
+            const candidate = argArray[0];
+            const transactionPrefixLength = (() => {
+              let count = 0;
+              while (
+                count < argArray.length &&
+                argArray[count] instanceof Transaction
+              ) {
+                count++;
+              }
+              return count;
+            })();
+            const invocationArgs =
+              transactionPrefixLength > 0
+                ? argArray.slice(transactionPrefixLength)
+                : argArray;
+
+            const activeTransaction =
+              candidate instanceof Transaction
+                ? candidate
+                : Transaction.contextTransaction(thisArg);
+
+            if (activeTransaction) {
+              const updatedTransaction: Transaction<any> = new Transaction(
+                target.name,
+                propertyKey,
+                async () => {
+                  try {
+                    return resolve(
+                      await Reflect.apply(
+                        obj,
+                        updatedTransaction.bindToTransaction(thisArg),
+                        invocationArgs
+                      )
+                    );
+                  } catch (e: unknown) {
+                    return reject(e);
+                  }
+                },
+                data.length ? data : undefined
+              );
+              activeTransaction.bindTransaction(updatedTransaction);
+              activeTransaction.fire();
+            } else {
+              const newTransaction: Transaction<R> = new Transaction(
+                target.name,
+                propertyKey,
+                async () => {
+                  try {
+                    return exitFunction(
+                      newTransaction,
+                      undefined,
+                      await Reflect.apply(
+                        obj,
+                        newTransaction.bindToTransaction(thisArg),
+                        invocationArgs
+                      )
+                    );
+                  } catch (e: unknown) {
+                    return exitFunction(newTransaction, e as Error);
+                  }
+                },
+                data.length ? data : undefined
+              );
+              Transaction.submit(newTransaction);
             }
-            return count;
-          })();
-          const invocationArgs =
-            transactionPrefixLength > 0
-              ? argArray.slice(transactionPrefixLength)
-              : argArray;
+          });
+        },
+      });
 
-          const activeTransaction =
-            candidate instanceof Transaction
-              ? candidate
-              : Transaction.contextTransaction(thisArg);
+      return descriptor;
+    };
+  }
 
-          if (activeTransaction) {
-            const updatedTransaction: Transaction<any> = new Transaction(
-              target.name,
-              propertyKey,
-              async () => {
-                try {
-                  return resolve(
-                    await Reflect.apply(
-                      obj,
-                      updatedTransaction.bindToTransaction(thisArg),
-                      invocationArgs
-                    )
-                  );
-                } catch (e: unknown) {
-                  return reject(e);
-                }
-              },
-              data.length ? data : undefined
-            );
-            activeTransaction.bindTransaction(updatedTransaction);
-            activeTransaction.fire();
-          } else {
-            const newTransaction: Transaction<R> = new Transaction(
-              target.name,
-              propertyKey,
-              async () => {
-                try {
-                  return exitFunction(
-                    newTransaction,
-                    undefined,
-                    await Reflect.apply(
-                      obj,
-                      newTransaction.bindToTransaction(thisArg),
-                      invocationArgs
-                    )
-                  );
-                } catch (e: unknown) {
-                  return exitFunction(newTransaction, e as Error);
-                }
-              },
-              data.length ? data : undefined
-            );
-            Transaction.submit(newTransaction);
-          }
-        });
-      },
-    });
-
-    return descriptor;
-  };
+  return Decoration.for(TransactionalKeys.TRANSACTIONAL)
+    .define({
+      decorator: innerTransactional,
+      args: data,
+    })
+    .apply();
 }
